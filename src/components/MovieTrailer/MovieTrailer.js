745f60042b1abc7c6ce89f8e96591ef1c050ba8e
hamzus2611 <hamza.gharsallah@isimg.tn>
2021-05-24 20:49:19 +0100

    second commit

--- src/components/MovieTrailer/MovieTrailer.js
+++ src/components/MovieTrailer/MovieTrailer.js
@@ -7,13 +7,16 @@ function MovieTrailer({ MovieListe, match }) {
     // const movie = movie.find((el) => el.id.toString() === match.params.id);
     console.log({ movie })
     return (
-        < div style={{ textAlign: "center" }}>
-           
-                {/* <iframe width="853" height="480" src={movie.trailer} title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe> */}
+        < div style={{ textAlign: "center" }} class="title">
+           <h1>{movie.title}</h1>
+           <br/>
+                <iframe width="853" height="480" src={movie.trailer}
+                 title="YouTube video player" frameborder="0" 
+                 allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" 
+                 allowfullscreen></iframe>
+
             
-{/*          
-            <h1>{movie.title}</h1>
-            <h1>{movie.trailer}</h1> */}
+            
             <br />
             <Link to="/">
 
